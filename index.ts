9e790e81bc1cd27a11aa0d7e16bfb24e482227aa
wibimipi <i@arborisov.ru>
2022-06-05 16:53:32 +0300

    edit url

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 
 const port = 8000
+const hostname = "http://5.63.155.234"
 
 const server = express()
 
@@ -9,6 +10,6 @@ server.get("/hello", (req, res) => {
 })
 
 
-server.listen(port, () => {
-    console.log(`Server starts on http://5.63.155.234:${port}`)
+server.listen(port, hostname, () => {
+    console.log(`Server starts on ${hostname}:${port}`)
 })
